60533bae46cb52599a071b283fcbd14270cdeb22
Shahzaib <51841651+m3zai6i@users.noreply.github.com>
2023-01-18 15:22:57 +0500

    exception handling

--- setup.py
+++ setup.py
@@ -74,89 +74,105 @@ if os.path.exists('Dataset/output/senerio_1'):
 
 imgs = glob.glob('Dataset/background_images/*.jpg')
 
-for bg_images in tqdm(imgs):
-    dir_fg_img_list = glob.glob('Dataset/input/*/*', recursive=True)
-    background_img_copy(bg_images, len(dir_fg_img_list))
-    base_name = os.path.basename(bg_images)
-    bg_images_ = cv2.imread(bg_images)
-    H, W, _ = bg_images_.shape
-    # print('Base Image Dim =',H,'x',W)
-    # cv2.imshow('image', bg_images_)
-    # cv2.waitKey(0)
+try:
 
-    #cv2.destroyAllWindows()
-    #dir_fg_img = popup_wind()
-    
-    dir_fg_img = dir_fg_img_list
+    for bg_images in tqdm(imgs):
+        dir_fg_img_list = glob.glob('Dataset/input/*/*', recursive=True)
+        background_img_copy(bg_images, len(dir_fg_img_list))
+        base_name = os.path.basename(bg_images)
+        bg_images_ = cv2.imread(bg_images)
+        H, W, _ = bg_images_.shape
+        # print('Base Image Dim =',H,'x',W)
+        # cv2.imshow('image', bg_images_)
+        # cv2.waitKey(0)
 
-    cv2.imshow('image', bg_images_)
-    cv2.setMouseCallback('image', coordinates_on_click)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
-    # print("coordinates",coordinates)
-    chunked_list = coordinates
-    chunk_size = 4
-    
-    lists = []
-    for chun in range(0, len(coordinates), chunk_size):
-        lists.append(coordinates[chun:chun+chunk_size])
+        #cv2.destroyAllWindows()
+        #dir_fg_img = popup_wind()
         
-    # print("lists",lists)
-    for kh in lists:
-        for img, ll in zip(dir_fg_img, list_bg_img):
+        dir_fg_img = dir_fg_img_list
+
+        cv2.imshow('image', bg_images_)
+        cv2.setMouseCallback('image', coordinates_on_click)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        # print("coordinates",coordinates)
+        chunked_list = coordinates
+        chunk_size = 4
+        
+        lists = []
+        for chun in range(0, len(coordinates), chunk_size):
+            lists.append(coordinates[chun:chun+chunk_size])
+            
+        # print("lists",lists)
+        for kh in lists:
+            for img, ll in zip(dir_fg_img, list_bg_img):
+                    
+                bg_base_name = os.path.basename(ll)
+                fg_base_name = os.path.basename(img)
                 
-            bg_base_name = os.path.basename(ll)
-            fg_base_name = os.path.basename(img)
-            
-            fg_img = Image.open(img, 'r').convert("RGBA")
-            bg_img = Image.open(ll, 'r').convert("RGBA")
+                fg_img = Image.open(img, 'r').convert("RGBA")
+                bg_img = Image.open(ll, 'r').convert("RGBA")
+                    
+                obj_img = Image.new('RGBA', (W, H), (0, 0, 0, 0))
+                obj_img.paste(bg_img, (0, 0))
+                fg_image_size = cv2.imread(img)
                 
-            obj_img = Image.new('RGBA', (W, H), (0, 0, 0, 0))
-            obj_img.paste(bg_img, (0, 0))
-            fg_image_size = cv2.imread(img)
-            
-            fh, fw, f_ = fg_image_size.shape
-            img_ = Image.open(img, 'r').convert("RGBA") 
-            
-            #try:
-            obj_img.paste(img_, (kh[0][0] - (fw//2), kh[0][1]  - (fh//2)), mask=img_)
-            obj_img_cv = np.array(obj_img)
-            obj_img_cv_bgr = cv2.cvtColor(obj_img_cv, cv2.COLOR_RGB2BGR)
-            # print('coordinates[0]',coordinates[0][0])
-            bb = pascal_voc_to_yolo((kh[0][0] - (fw//2)), (kh[0][1]  - (fh//2)),
-                                    (kh[0][0])+(fw//2), (kh[0][1])+ (fh//2), W, H)
-            #cv2.rectangle(obj_img_cv_bgr, (kh[0][0], kh[0][1]), ((kh[0][0])+fw,(kh[0][1])+fh), (0, 0, 255), 1)
+                fh, fw, f_ = fg_image_size.shape
+                img_ = Image.open(img, 'r').convert("RGBA") 
+                
+                #try:
+                obj_img.paste(img_, (kh[0][0] - (fw//2), kh[0][1]  - (fh//2)), mask=img_)
+                obj_img_cv = np.array(obj_img)
+                obj_img_cv_bgr = cv2.cvtColor(obj_img_cv, cv2.COLOR_RGB2BGR)
+                # print('coordinates[0]',coordinates[0][0])
+                bb = pascal_voc_to_yolo((kh[0][0] - (fw//2)), (kh[0][1]  - (fh//2)),
+                                        (kh[0][0])+(fw//2), (kh[0][1])+ (fh//2), W, H)
+                #cv2.rectangle(obj_img_cv_bgr, (kh[0][0], kh[0][1]), ((kh[0][0])+fw,(kh[0][1])+fh), (0, 0, 255), 1)
 
-            class_no = img.split('\\')[-2][0]
-            # print(img)
-            # print(class_no)
+                class_no = img.split('\\')[-2][0]
+                # print(img)
+                # print(class_no)
 
 
-            txt_file = open('Dataset/output/'+bg_base_name[:-4]+'.txt', 'a')
-            txt_file.write(class_no+" "+str(bb[0])+' '+str(bb[1])+' '+str(bb[2])+' '+str(bb[3])+'\n')
-            txt_file.close()
+                txt_file = open('Dataset/output/'+bg_base_name[:-4]+'.txt', 'a')
+                txt_file.write(class_no+" "+str(bb[0])+' '+str(bb[1])+' '+str(bb[2])+' '+str(bb[3])+'\n')
+                txt_file.close()
 
-            cv2.imwrite('Dataset/output/'+bg_base_name,obj_img_cv_bgr)
-                # i += 1
-            # except:
-            #     pass
-            
-        # list_bg_img = glob.glob('C:/Users/Shahzaib/Desktop/Doing/datageneration/senerio_3/Dataset/output/*.jpg')       
-        # list_bg_img = glob.glob('C:/Users/Shahzaib/Documents/GitHub/Fast-Real-Synthetic-Dataset-Generation-Tool/Dataset/output/*.jpg')
-        list_bg_img = glob.glob('./Dataset/output/*.jpg')
-    
-    for cop in list_bg_img:
-        b_n = os.path.basename(cop)
-        os.rename(cop,'Dataset/output/senerio_1/'+b_n)
-        os.rename(cop[:-4]+'.txt', 'Dataset/output/senerio_1/'+b_n[:-4]+".txt")
+                cv2.imwrite('Dataset/output/'+bg_base_name,obj_img_cv_bgr)
+                    # i += 1
+                # except:
+                #     pass
+                
+            # list_bg_img = glob.glob('C:/Users/Shahzaib/Desktop/Doing/datageneration/senerio_3/Dataset/output/*.jpg')       
+            # list_bg_img = glob.glob('C:/Users/Shahzaib/Documents/GitHub/Fast-Real-Synthetic-Dataset-Generation-Tool/Dataset/output/*.jpg')
+            list_bg_img = glob.glob('./Dataset/output/*.jpg')
+        
+        for cop in list_bg_img:
+            b_n = os.path.basename(cop)
+            os.rename(cop,'Dataset/output/senerio_1/'+b_n)
+            os.rename(cop[:-4]+'.txt', 'Dataset/output/senerio_1/'+b_n[:-4]+".txt")
 
 
-    list_bg_img.clear()
-    dir_fg_img.clear()
-    coordinates.clear()
+        list_bg_img.clear()
+        dir_fg_img.clear()
+        coordinates.clear()
 
-if os.path.exists('Dataset/list_image'):
-    shutil.rmtree('Dataset/list_image')
-    os.mkdir('Dataset/list_image')
-    
+except:
+
+    if os.path.exists('Dataset/list_image'):
+        shutil.rmtree('Dataset/list_image')
+        os.mkdir('Dataset/list_image')
+
+    # Post Processing Script
+    all_files = glob.glob('./Dataset/output/senerio_1/*.*')
+
+    # remove space from file name
+    for file in all_files:
+        os.rename(file, file.replace(" ", "_"))
+
+    all_files = glob.glob('Dataset/output/senerio_1/*.*')
+    for file in tqdm(all_files, desc="Processing"):
+        os.rename(file, file.replace(".", "_"+date_time_id+"."))
+
+
